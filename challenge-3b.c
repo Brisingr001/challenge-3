e6f646184df16d2616c1c3f6bb89f990cf57ef4f
H Pranav Nayak <hpranavnayak@gmail.com>
2023-06-30 22:26:23 +0530

    added unity files and small changes to 3b

--- challenge-3b.c
+++ challenge-3b.c
@@ -15,13 +15,19 @@ int main(){
     uint8_t arr[MAX_DATA_LENGTH]={0};
     int j=0;
     char *str=(char *)calloc(MAX_DATA_LENGTH+1, sizeof(char));
+    
     printf("Enter the data to transmit\n");
     fgets(str,MAX_DATA_LENGTH+1,stdin);
+    
     while(str[j]!='\0')
         arr[j++]=*(str+j);
+    
+    
 
     // for(int k=0;k<MAX_DATA_LENGTH;k++)
-    //     printf("%c",arr[k]);
+    //     printf("%x\t",arr[k]);
+    // printf("\n");
+
     
 
 }
